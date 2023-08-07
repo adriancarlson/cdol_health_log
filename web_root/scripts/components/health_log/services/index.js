260da8563e25cc5638938df7881383895895f564
Adrian Carlson <adrian.r.carlson@gmail.com>
2023-08-07 14:59:43 -0500

    2.12 - services linked up

--- web_root/scripts/components/health_log/services/index.js
+++ web_root/scripts/components/health_log/services/index.js
@@ -1,4 +1,6 @@
 'use strict'
 define(function (require) {
 	require('components/health_log/services/pqService')
+	require('components/health_log/services/psApiService')
+	require('components/health_log/services/formatService')
 })
--- web_root/scripts/components/health_log/services/psApiService.js
+++ web_root/scripts/components/health_log/services/psApiService.js
@@ -1,85 +1,94 @@
-define(['angular'], function (angular) {
-	angular.module('psApiModule', ['formatService']).service('psApiService', function ($http, $q, formatService) {
-		this.psApiCall = (tableName, method, payload, recId) => {
-			let deferredResponse = $q.defer()
-			tableName = tableName.toLowerCase()
-			let path = `/ws/schema/table/${tableName}`
-			let url = `${path}${recId ? `/${recId}` : ''}`
-			headers = {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			}
-			let httpObject = {
-				url: `${url}`,
-				method: method,
-				headers: headers
-			}
-			// copying payload using spread, to keep original payload object in tact. apiPayload is what will be submitted with any API call below.
-			let apiPayload = { ...payload }
-			// Unique Headers
-			switch (method) {
-				//Create
-				case 'POST':
-				case 'PUT':
-					if (apiPayload.dateKeys) {
-						apiPayload = formatService.objIterator(apiPayload, apiPayload.dateKeys, 'formatDateForApi')
+'use strict'
+define(function (require) {
+	var module = require('components/health_log/module')
+	module.factory('psApiModule', [
+		'$http',
+		'$q',
+		'formatService',
+		function ($http, $q, formatService) {
+			return {
+				psApiCall: (tableName, method, payload, recId) => {
+					let deferredResponse = $q.defer()
+					tableName = tableName.toLowerCase()
+					let path = `/ws/schema/table/${tableName}`
+					let url = `${path}${recId ? `/${recId}` : ''}`
+					headers = {
+						Accept: 'application/json',
+						'Content-Type': 'application/json'
 					}
-					delete apiPayload.dateKeys
-					if (apiPayload.checkBoxKeys) {
-						apiPayload = formatService.objIterator(apiPayload, apiPayload.checkBoxKeys, 'formatChecksForApi')
+					let httpObject = {
+						url: `${url}`,
+						method: method,
+						headers: headers
 					}
-					delete apiPayload.checkBoxKeys
-					if (apiPayload.titleKeys) {
-						apiPayload = formatService.objIterator(apiPayload, apiPayload.titleKeys, 'titleCase')
-					}
-					delete apiPayload.titleKeys
-					if (apiPayload.sentenceKeys) {
-						apiPayload = formatService.objIterator(apiPayload, apiPayload.sentenceKeys, 'sentenceCase')
-					}
-					delete apiPayload.sentenceKeys
-					if (apiPayload.deleteKeys) {
-						apiPayload = formatService.objIterator(apiPayload, apiPayload.deleteKeys, 'deleteKeys')
-					}
-					delete apiPayload.deleteKeys
-					const data = { tables: {} }
-					data.tables[tableName] = apiPayload
-					httpObject['data'] = data
-					break
-				//READ
-				case 'GET':
-					httpObject['params'] = {
-						projection: '*'
-					}
-					break
-			}
-
-			$http(httpObject).then(
-				res => {
+					// copying payload using spread, to keep original payload object in tact. apiPayload is what will be submitted with any API call below.
+					let apiPayload = { ...payload }
+					// Unique Headers
 					switch (method) {
+						//Create
 						case 'POST':
 						case 'PUT':
-							deferredResponse.resolve(res.data.record || [])
-							break
-						case 'GET':
-							resData = res.data.tables[tableName]
 							if (apiPayload.dateKeys) {
-								resData = formatService.objIterator(resData, apiPayload.dateKeys, 'formatDateFromApi')
+								apiPayload = formatService.objIterator(apiPayload, apiPayload.dateKeys, 'formatDateForApi')
 							}
+							delete apiPayload.dateKeys
 							if (apiPayload.checkBoxKeys) {
-								resData = formatService.objIterator(resData, apiPayload.checkBoxKeys, 'formatChecksFromApi')
+								apiPayload = formatService.objIterator(apiPayload, apiPayload.checkBoxKeys, 'formatChecksForApi')
 							}
-							deferredResponse.resolve(resData)
+							delete apiPayload.checkBoxKeys
+							if (apiPayload.titleKeys) {
+								apiPayload = formatService.objIterator(apiPayload, apiPayload.titleKeys, 'titleCase')
+							}
+							delete apiPayload.titleKeys
+							if (apiPayload.sentenceKeys) {
+								apiPayload = formatService.objIterator(apiPayload, apiPayload.sentenceKeys, 'sentenceCase')
+							}
+							delete apiPayload.sentenceKeys
+							if (apiPayload.deleteKeys) {
+								apiPayload = formatService.objIterator(apiPayload, apiPayload.deleteKeys, 'deleteKeys')
+							}
+							delete apiPayload.deleteKeys
+							const data = { tables: {} }
+							data.tables[tableName] = apiPayload
+							httpObject['data'] = data
 							break
-						case 'DELETE':
-							deferredResponse.resolve(res)
+						//READ
+						case 'GET':
+							httpObject['params'] = {
+								projection: '*'
+							}
 							break
 					}
-				},
-				res => {
-					psAlert({ message: `There was an error ${method}ing the data to ${tableName}`, title: `${method} Error` })
+
+					$http(httpObject).then(
+						res => {
+							switch (method) {
+								case 'POST':
+								case 'PUT':
+									deferredResponse.resolve(res.data.record || [])
+									break
+								case 'GET':
+									resData = res.data.tables[tableName]
+									if (apiPayload.dateKeys) {
+										resData = formatService.objIterator(resData, apiPayload.dateKeys, 'formatDateFromApi')
+									}
+									if (apiPayload.checkBoxKeys) {
+										resData = formatService.objIterator(resData, apiPayload.checkBoxKeys, 'formatChecksFromApi')
+									}
+									deferredResponse.resolve(resData)
+									break
+								case 'DELETE':
+									deferredResponse.resolve(res)
+									break
+							}
+						},
+						res => {
+							psAlert({ message: `There was an error ${method}ing the data to ${tableName}`, title: `${method} Error` })
+						}
+					)
+					return deferredResponse.promise
 				}
-			)
-			return deferredResponse.promise
+			}
 		}
-	})
+	])
 })
