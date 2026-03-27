8f0493f8facf79da844bbec88a6ffb55c40aebc8
Adrian Carlson <adrian.r.carlson@gmail.com>
2026-03-27 14:42:03 -0500

    refactor medication inventory controller

--- web_root/admin/students/medication/medication.js
+++ web_root/admin/students/medication/medication.js
@@ -2,13 +2,11 @@ define(['angular', 'components/shared/powerschoolModule'], angular => {
 	'use strict'
 	const medicationModule = angular.module('medicationModule', ['powerSchoolModule'])
 
-	medicationModule.controller('medicationInventoryController', function ($scope, $rootScope, $attrs) {
+	medicationModule.controller('inventoryController', function ($scope, $rootScope, $attrs) {
 		$j(document).dblclick(() => console.log($scope))
 
 		$rootScope.appData = {
 			context: $attrs.ngContext
 		}
-
-		$scope.message = 'This is the medication inventory plugin. It is currently under construction.'
 	})
 })
